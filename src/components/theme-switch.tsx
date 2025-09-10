db08f8c3e70e985ad5a1eea1a7bddd5e108088e0
Ashish <karnashish5115@gmail.com>
2025-09-10 15:54:39 +0000

    the button for theme switcher is not visible

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -16,7 +16,7 @@ export function ThemeSwitch() {
 
   if (!mounted) {
     return (
-      <Button variant="ghost" size="icon" className="w-9 h-9" disabled>
+      <Button variant="outline" size="icon" className="w-9 h-9" disabled>
         <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
         <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
@@ -29,7 +29,7 @@ export function ThemeSwitch() {
   }
 
   return (
-    <Button variant="ghost" size="icon" onClick={toggleTheme} className="w-9 h-9">
+    <Button variant="outline" size="icon" onClick={toggleTheme} className="w-9 h-9">
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
